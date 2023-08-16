414172c84be7143c82a21307e86e51c4a7e60ff9
Noah Beach <noah@omnicommander.com>
2023-08-16 13:17:04 -0400

    Async

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use daemonize::Daemonize;
-use std::fs::File;
-use std::{thread, time};
-use reqwest::blocking::{get, Client};
-use serde::{Serialize, Deserialize};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use std::{thread, time, fs::File, rc::Rc};
+use anyhow::Result;
 
 #[derive(Serialize, Deserialize)]
 struct Apikey {
-    key: String
+    key: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -15,52 +15,62 @@ struct Video {
     url: String,
 }
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = "http://localhost:8080";
+    let id = "fd7b5f34-0b07-4ea1-8840-a577c8a5a4ed";
+
+    // Wrap the client in an Rc so we're not using unnecessary memory
+    let client = Rc::new(Client::new());
+
     let stdout = File::create("/tmp/signage.out")?;
     let stderr = File::create("/tmp/signage.err")?;
 
-
-    let daemonize = Daemonize::new()
+    // Start the daemon
+    Daemonize::new()
         .pid_file("/tmp/signage.pid")
         .chown_pid_file(true)
         .working_directory("/tmp")
         .stdout(stdout)
-        .stderr(stderr);
+        .stderr(stderr)
+        .start()?;
 
-    match daemonize.start() {
-        Ok(_) => println!("Success, daemonized"),
-        Err(e) => eprintln!("Error: {}", e)
-    }
-
-    let sleep_duration = time::Duration::new(30, 0);
-    let mut mpv_handle: Option<std::process::Child> = None;
+    let mut mpv: Option<std::process::Child> = None;
 
     loop {
-        get_videos(authenticate());
+        get_videos(client.clone(), url, id, authenticate(client.clone(), url, id).await?).await?;
 
-        if mpv_handle.is_some() {
-            mpv_handle.unwrap().kill()?;
+        if mpv.is_some() {
+            mpv.unwrap().kill()?;
         }
 
-        mpv_handle = Some(std::process::Command::new("mpv")
-            .arg("--loop")
-            .arg("/home/noah/Downloads/icelandwaterfall.mp4")
-            .spawn()?);
+        mpv = Some(
+            std::process::Command::new("mpv")
+                .arg("--loop")
+                .arg("/home/noah/Downloads/icelandwaterfall.mp4")
+                .spawn()?,
+        );
 
-        thread::sleep(sleep_duration);
+        thread::sleep(time::Duration::new(30, 0));
     }
 }
 
-fn authenticate() -> Apikey {
-    let apikey: Apikey = get("http://localhost:8080/get-new-key/fd7b5f34-0b07-4ea1-8840-a577c8a5a4ed").unwrap()
-        .json().unwrap();
+async fn authenticate(client: Rc<Client>, url: &str, id: &str) -> Result<Apikey> {
+    let apikey: Apikey = client.get(String::from(url) + "/get-new-key/" + id).send().await?.json().await?;
 
     println!("{}", apikey.key);
-    apikey
+    Ok(apikey)
 }
 
-fn get_videos(apikey: Apikey) {
-    let videos: Vec<Video> = Client::new().get("http://localhost:8080/recieve-videos/fd7b5f34-0b07-4ea1-8840-a577c8a5a4ed").header("APIKEY", apikey.key).send().unwrap().json().unwrap();
+async fn get_videos(client: Rc<Client>, url: &str, id: &str, apikey: Apikey) -> Result<()> {
+    let videos: Vec<Video> = client 
+        .get(String::from(url) + "/recieve-videos/" + id)
+        .header("APIKEY", apikey.key)
+        .send()
+        .await?
+        .json()
+        .await?;
 
     println!("{:?}", videos);
+    Ok(())
 }
