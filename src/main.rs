035c6431b5916e45f2b6a1e621da0133dc484c2d
OMNICOMMANDER <33461718+omnicommander@users.noreply.github.com>
2024-07-09 12:00:55 -0500

    Updated Main

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::new();
 
     // Load the configs
+    println!("Loading configuration...");
     config.load().await?;
+    println!("Loading data...");
     data.load().await?;
 
     let _ = wait_for_api(&client, &config).await;
     
     // Get our api key
     if config.key.is_none() {
+        println!("API key is not set. Requesting a new API key...");
         config.key = Some(get_new_key(&client, &config).await?.key);
     }
+    
+    // Print the API key
+    if let Some(api_key) = &config.key {
+        println!("API Key: {}", api_key);
+    }
+
     config.write().await?;
 
     // Get the videos if we've never updated
@@ -54,20 +63,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
         match mpv.try_wait() {
             Ok(Some(_)) => mpv = start_mpv().await?,
             Ok(None) => (),
-            Err(error) => eprintln!("{error}"),
+            Err(error) => eprintln!("Error waiting for mpv process: {error}"),
         }
     }
 }
 
-/// Loops until we get a response from the api to make sure our network is online
+/// Loops until we get a response from the API to make sure our network is online
 async fn wait_for_api(client: &Client, config: &Config) -> Result<bool, Box<dyn Error>> {
+    println!("Waiting for API to become available...");
     let mut interval = time::interval(Duration::from_secs(1));
     loop {
         let res = client.get(format!("{}/health", config.url)).send().await;
-        if res.is_ok() {
-            match res.unwrap().status() {
+        if let Ok(response) = res {
+            match response.status() {
                 StatusCode::OK => break,
                 StatusCode::INTERNAL_SERVER_ERROR => {
+                    println!("Server error. Retrying in 2 minutes...");
                     time::interval(Duration::from_secs(120)).tick().await;
                 },
                 _ => (),
@@ -76,24 +87,27 @@ async fn wait_for_api(client: &Client, config: &Config) -> Result<bool, Box<dyn 
         interval.tick().await;
     }
 
+    println!("API is available.");
     Ok(true)
 }
 
 /// Starts the mpv player with the proper playlist and flags
 async fn start_mpv() -> Result<Child, Box<dyn Error>> {
+    println!("Starting mpv player...");
     let child = Command::new("mpv")
-        // .arg("-fs")
         .arg("--loop-playlist=inf")
         .arg("--volume=-1")
         .arg("--no-terminal")
         .arg(format!("--playlist={}/.local/share/signage/playlist.txt", std::env::var("HOME")?))
         .spawn()?;
 
+    println!("mpv player started.");
     Ok(child)
 }
 
-/// Makes the proper request to recieve an apikey
+/// Makes the proper request to receive an API key
 async fn get_new_key(client: &Client, config: &Config) -> Result<Apikey, Box<dyn Error>> {
+    println!("Requesting a new API key...");
     let res: Apikey = client
         .get(format!("{}/get-new-key/{}", config.url, config.id))
         .basic_auth(&config.username, Some(&config.password))
@@ -102,47 +116,75 @@ async fn get_new_key(client: &Client, config: &Config) -> Result<Apikey, Box<dyn
         .json()
         .await?;
 
-    println!("{}", res.key);
+    println!("Received new API key: {}", res.key);
     Ok(res)
 }
 
-/// Makes the proper request to recieve the last time the connected group was updated
+/// Makes the proper request to receive the last time the connected group was updated
 async fn sync(client: &Client, config: &Config) -> Result<Option<DateTime<Utc>>, Box<dyn Error>> {
+    println!("Syncing with the server...");
     let res: Updated = client
         .get(format!("{}/sync/{}", config.url, config.id))
-        .header("APIKEY", config.key.clone().unwrap())
+        .header("APIKEY", config.key.clone().unwrap_or_default())
         .send()
         .await?
         .json()
         .await?;
 
-    println!("{:?}", res.updated);
+    println!("Last updated: {:?}", res.updated);
 
     Ok(res.updated)
 }
 
-/// Makes the proper request to recieve the list of videos
-async fn recieve_videos(client: &Client, config: &Config) -> Result<Vec<Video>, Box<dyn Error>> {
-    let res: Vec<Video> = client
-        .get(format!("{}/recieve-videos/{}", config.url, config.id))
-        .header("APIKEY", config.key.clone().unwrap())
+/// Makes the proper request to receive the list of videos
+async fn receive_videos(client: &Client, config: &Config) -> Result<Vec<Video>, Box<dyn Error>> {
+    println!("Receiving videos...");
+
+    let url = format!("{}/recieve-videos/{}", config.url, config.id);
+    let standard_api_key = config.key.clone().unwrap_or_default();
+
+    // Request a new authorization token
+    let new_key = get_new_key(client, config).await?;
+    let auth_token = new_key.key;
+
+    // Print the values for debugging
+    println!("Request URL: {}", url);
+    println!("Standard API Key: {}", standard_api_key);
+    println!("Authorization Token: {}", auth_token);
+
+    let response = client
+        .get(&url)
+        .header("Accept", "application/json")
+        .header("Cache-Control", "no-cache")
+        .header("Accept-Encoding", "gzip, deflate, br")
+        .header("Connection", "keep-alive")
+        .header("APIKEY", auth_token)
         .send()
-        .await?
-        .json()
         .await?;
 
-    println!("{res:?}");
-    Ok(res)
+    let status = response.status();
+    let text = response.text().await?;
+
+    println!("Response status: {}", status);
+    println!("Response body: {}", text);
+
+    if status.is_success() {
+        let res: Vec<Video> = serde_json::from_str(&text)?;
+        println!("Received videos: {:?}", res);
+        Ok(res)
+    } else {
+        Err(format!("Failed to receive videos: {}", text).into())
+    }
 }
 
-/// Recieves and downloads videos and writes to the playlist file
+/// Receives and downloads videos and writes to the playlist file
 async fn update_videos(
     client: &Client,
     config: &Config,
     data: &mut Data,
     updated: Option<DateTime<Utc>>,
 ) -> Result<(), Box<dyn Error>> {
-    data.videos = recieve_videos(client, config).await?;
+    data.videos = receive_videos(client, config).await?;
     data.last_update = updated;
     data.write().await?;
     
@@ -150,6 +192,7 @@ async fn update_videos(
 
     // Remove the playlist file
     if Path::new(&format!("{home}/.local/share/signage/playlist.txt")).try_exists()? {
+        println!("Removing old playlist file...");
         tokio::fs::remove_file(format!("{home}/.local/share/signage/playlist.txt")).await?;
     }
 
@@ -166,11 +209,13 @@ async fn update_videos(
         }
 
         // Download the video
+        println!("Downloading video: {}", video.title);
         video.download(client).await?;
 
         // Write the path to the playlist file
         file.write_all(format!("{}/.local/share/signage/{}.mp4\n", home, video.title).as_bytes()).await?;
     }
 
+    println!("Updated playlist file.");
     Ok(())
 }
