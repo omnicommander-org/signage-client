b96531dfeb1268404c539d5ecf146c5f057998e1
OMNICOMMANDER <33461718+omnicommander@users.noreply.github.com>
2024-07-10 10:04:06 -0500

    Dev Merge

--- src/config.rs
+++ src/config.rs
@@ -16,7 +16,7 @@ impl Config {
         Config::default()
     }
 
-    /// Loads `Config` from $HOME/config/signage/signage.json
+    /// Loads `Config` from $HOME/.config/signage/signage.json
     pub async fn load(&mut self) -> Result<(), Box<dyn Error>> {
         load_json(
             self,
@@ -26,12 +26,21 @@ impl Config {
         .await
     }
 
-    /// Writes `Config` to $HOME/config/signage/signage.json
+    /// Writes `Config` to $HOME/.config/signage/signage.json
     pub async fn write(&self) -> Result<(), Box<dyn Error>> {
+        let json_content = serde_json::to_string_pretty(self)?;
+        println!("Writing to signage.json: {}", json_content);
+        
         write_json(
             self,
             &format!("{}/.config/signage/signage.json", env::var("HOME")?),
         )
         .await
+    }
+
+    /// Updates the API key and writes the updated configuration to the file
+    pub async fn update_key(&mut self, new_key: String) -> Result<(), Box<dyn Error>> {
+        self.key = Some(new_key);
+        self.write().await
     }
 }
