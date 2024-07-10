5aa4558677d85e4ca13fbfc281f0ac1ccbcab8fa
OMNICOMMANDER <33461718+omnicommander@users.noreply.github.com>
2024-07-10 10:03:21 -0500

    Dev Merge

--- src/config.rs
+++ src/config.rs
@@ -28,9 +28,6 @@ impl Config {
 
     /// Writes `Config` to $HOME/config/signage/signage.json
     pub async fn write(&self) -> Result<(), Box<dyn Error>> {
-        let json_content = serde_json::to_string_pretty(self)?;
-        println!("Writing to signage.json: {}", json_content);
-        
         write_json(
             self,
             &format!("{}/.config/signage/signage.json", env::var("HOME")?),
