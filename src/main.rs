c3fbdc81c86ad6900a994b66bf9a209a0d13351b
OMNICOMMANDER <33461718+omnicommander@users.noreply.github.com>
2024-08-01 13:59:10 -0500

    Integrate Reporting

--- src/main.rs
+++ src/main.rs
@@ -115,6 +115,9 @@ async fn start_mpv() -> Result<Child, Box<dyn Error>> {
 }
 
 async fn get_new_key(client: &Client, config: &mut Config) -> Result<Apikey, Box<dyn Error>> {
+    println!("Loading configuration...");
+    config.load().await?;
+    println!("Reloading data...------------");
     println!("Requesting a new API key...{}", config.id);
     let res: Apikey = client
         .get(format!("{}/get-new-key/{}", config.url, config.id))
