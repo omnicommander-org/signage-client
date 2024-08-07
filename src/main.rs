29ffcbf4006c50b5a0688540dc4d401395ff3719
OMNICOMMANDER <33461718+omnicommander@users.noreply.github.com>
2024-08-07 08:37:49 -0500

    Updated mpv start

--- src/main.rs
+++ src/main.rs
@@ -45,8 +45,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let mut interval = time::interval(Duration::from_secs(20));
     let mut metrics_interval = time::interval(Duration::from_secs(1800));
-/*     let mut mpv = start_mpv().await?; */
-
+    let mut mpv = start_mpv().await?;
+    mpv.kill().await?;
     let mut terminate = signal(SignalKind::terminate())?;
     let mut interrupt = signal(SignalKind::interrupt())?;
     let mut hup = signal(SignalKind::hangup())?;
